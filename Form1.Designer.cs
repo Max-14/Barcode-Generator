423ec735ab08b96f93d56638c733b8ac6f5d375d
Max-14 <mulyatalukona@gmail.com>
2023-09-29 15:03:14 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Barcode_Generator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.printDialog1 = new System.Windows.Forms.PrintDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button1.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("SamsungOneUI Medium Condensed", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Blue;
+            this.button1.Location = new System.Drawing.Point(141, 15);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(173, 52);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generate Barcode";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 144);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 20, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 105);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // printDocument1
+            // 
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
+            // 
+            // printDialog1
+            // 
+            this.printDialog1.UseEXDialog = true;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("SamsungOneUI Medium Condensed", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.button2.Location = new System.Drawing.Point(179, 82);
+            this.button2.Margin = new System.Windows.Forms.Padding(5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 45);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Print Barcode";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.Azure;
+            this.ClientSize = new System.Drawing.Size(473, 317);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ZNPHI Barcode Generator         | @MaxLukona";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+        private System.Windows.Forms.PrintDialog printDialog1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZXing;
+using IniParser;
+using IniParser.Model;
+
+namespace Barcode_Generator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            // Read data from the INI file.
+            string ProjectCode = ReadIniValue("BarcodeData", "ProjectCode");
+            string CenterCode = ReadIniValue("BarcodeData", "CenterCode");
+
+            // Generate a combined string with date, time, and INI data.
+            string combinedData = $"{ProjectCode}-{CenterCode}-{DateTime.Now.ToString("yyyy-MM-dd-HH:mm:ss")}";
+
+
+            if (!string.IsNullOrEmpty(combinedData))
+                {
+                    // Create a BarcodeWriter instance.
+                    BarcodeWriter barcodeWriter = new BarcodeWriter();
+
+                    // Set encoding properties (you can customize these as needed).
+                    barcodeWriter.Format = BarcodeFormat.CODE_128 ; // You can choose different barcode formats.
+                    barcodeWriter.Options = new ZXing.Common.EncodingOptions
+                    {
+                        Width = 100,  // Adjust the width of the barcode image.
+                        Height = 170  // Adjust the height of the barcode image.
+                    };
+
+                    // Generate the barcode image.
+                    pictureBox1.Image = barcodeWriter.Write(combinedData);
+                }
+                else
+                {
+                    MessageBox.Show("Failed to generate barcode. Data is empty.");
+                }
+        }
+
+        // Read a value from the INI file.
+        private string ReadIniValue(string section, string key)
+        {
+            // Get the path to the directory containing the executable.
+            string executablePath = AppDomain.CurrentDomain.BaseDirectory;
+
+            // Combine the executable path with the name of your INI file.
+            string iniFilePath = Path.Combine(executablePath, "bcgen.ini"); // Specify the path to your INI file.
+            string defaultValue = ""; // Default value if key is not found.
+
+            if (File.Exists(iniFilePath))
+            {
+                var parser = new FileIniDataParser();
+                IniData data = parser.ReadFile(iniFilePath);
+
+                if (data.Sections.ContainsSection(section))
+                {
+                    if (data[section].ContainsKey(key))
+                    {
+                        return data[section][key];
+                    }
+                }
+            }
+
+            return defaultValue;
+        }
+
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            // Get the barcode image from the PictureBox.
+            Image barcodeImage = pictureBox1.Image;
+
+            // Draw the barcode image on the print page.
+            e.Graphics.DrawImage(barcodeImage, new Point(100, 100)); // Adjust the coordinates as needed.
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Display the PrintDialog to allow the user to choose a printer.
+            if (printDialog1.ShowDialog() == DialogResult.OK)
+            {
+                // Set the selected printer for the PrintDocument.
+                printDocument1.PrinterSettings = printDialog1.PrinterSettings;
+
+                // Print the document.
+                printDocument1.Print();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
